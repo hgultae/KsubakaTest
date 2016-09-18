c2dfbe1280f4f3995f5e524a6bd630618d922dbc
Hurman Gul <hurman.gul.theappexperts@gmail.com>
2016-09-18 15:31:26 +0100

    Updating UI for list items. Added error drawable incase image cant be loaded

--- app/src/main/java/com/hg/ksubakatest/ui/MainActivity.java
+++ app/src/main/java/com/hg/ksubakatest/ui/MainActivity.java
@@ -3,14 +3,19 @@ package com.hg.ksubakatest.ui;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.hg.ksubakatest.MyApplication;
 import com.hg.ksubakatest.R;
 import com.hg.ksubakatest.model.FilmList;
 import com.hg.ksubakatest.model.Search;
 import com.hg.ksubakatest.webservice.FilmService;
+import com.jakewharton.rxbinding.widget.RxTextView;
 
 import javax.inject.Inject;
 
@@ -26,8 +31,10 @@ public class MainActivity extends AppCompatActivity {
     FilmService mService;
 
     public final String TAG = getClass().getSimpleName();
-    private ProgressDialog mDialog;
+    private ProgressDialog progressDialog;
     private EditText mFieldMovieName;
+    private RecyclerView mRecyclerView;
+    private FilmListAdapter mFilmListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         ((MyApplication) getApplication()).getmApiComponent().inject(this);
 
         mFieldMovieName = (EditText) findViewById(R.id.field_movie_name);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_film_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        showProgressDialog("", "Loading...");
         Observable<FilmList> filmListObservable = mService.getFilmList("Batman", "json", "movie");
 
         filmListObservable.subscribeOn(Schedulers.newThread())
@@ -57,12 +67,40 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onNext(FilmList filmList) {
 
+                        hideProgressDialog();
                         Log.i(TAG, "size of list: " + filmList.getSearch().size());
 
-                        for(Search search : filmList.getSearch()){
-                            Log.i(TAG, "search item: " + search);
-                        }
+                        mFilmListAdapter = new FilmListAdapter(MainActivity.this, filmList, R.layout.row_film_list);
+                        mRecyclerView.setAdapter(mFilmListAdapter);
+
+                        mFilmListAdapter.setOnItemClickListener(new FilmListAdapter.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                                Toast.makeText(MainActivity.this, "you clicked: " + position, Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
                     }
                 });
+
+
     }
+
+    protected void showProgressDialog(String title, String description) {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(true);
+        progressDialog.setTitle(title);
+        progressDialog.setMessage(description);
+        progressDialog.show();
+    }
+
+    protected void hideProgressDialog() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
+
 }
